545755fe3b3b7734b2bc9b84e2fdced29b3e14f2
Alexsandr Chigarev <majestik13@mail.ru>
2022-09-16 15:19:39 +0300

    Photo

--- components/Person.js
+++ components/Person.js
@@ -29,12 +29,13 @@ class Person {
   }
 }
 
+
 function lookPhoto(evt) {
-  photo.src = evt.target.src;
-  photo.alt = evt.target.alt;
-  photo.textContent = evt.target.alt;
+    photo.src = evt.target.src;
+    photo.alt = evt.target.alt;
+    photo.textContent = evt.target.alt;
+  return setTimeout(lookPhoto, 6000)
 }
-setTimeout(lookPhoto, 3000);
 
 function createPersons(personList, list) {
   personList.forEach((item) => {
